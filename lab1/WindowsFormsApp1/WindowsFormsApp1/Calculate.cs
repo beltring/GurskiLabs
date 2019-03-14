af94a3834c508b23bc83eac7a03bb3584b8dd94b
Pavel Boltromyuk <beltring@tut.by>
2019-03-14 10:35:55 +0300

    Laboratory work 1

---
+++ lab1/WindowsFormsApp1/WindowsFormsApp1/Calculate.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    class Calculate
+    {
+        public static double Func(double x)
+        {
+            return Math.Sqrt(1 - 0.5 * Math.Sin(x) * Math.Sin(x));
+        }
+
+        public static double Trapezium(double a, double b, double step)
+        {
+            double result = 0;
+
+            for (double x = a; x <= b; x += step)
+            {
+                result += step * (Func(x) + Func(x + step)) / 2;
+            }
+
+            return result;
+        }
+        public static double LeftRect(double a, double b, double step)
+        {
+            double result = 0;
+
+            for (double x = a; x <= b; x += step)
+            {
+                result += Func(x) * step;
+            }
+
+            return result;
+        }
+        public static double RightRect(double a, double b, double step)
+        {
+            double result = 0;
+
+            for (double x = a; x <= b; x += step)
+            {
+                result += Func(x + step) * step;
+            }
+
+            return result;
+        }
+        public static double MiddleRect(double a, double b, double step)
+        {
+            double result = 0;
+
+            for (double x = a; x <= b; x += step)
+            {
+                result += Func(x + step / 2) * step;
+            }
+
+            return result;
+        }
+        public static void DrawMiddleRect(ref Chart chart1, double a, double b, double step)
+        {
+            chart1.Series[0].Points.Clear();
+            double y;
+
+            for (double x = a; x <= b; x += step)
+            {
+                y = Func(x + step / 2);
+
+                chart1.Series[0].Points.AddXY(x, y);
+                if (x + step <= b)
+                {
+                    chart1.Series[0].Points.AddXY(x + step, y);
+                }
+            }
+        }
+
+        public static void DrawTrapezium(ref Chart chart1, double a, double b, double step)
+        {
+            chart1.Series[0].Points.Clear();
+            double y;
+
+            for (double x = a; x <= b; x += step)
+            {
+                y = Func(x);
+
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+        }
+        public static void DrawLeftRect(ref Chart chart1, double a, double b, double step)
+        {
+            chart1.Series[0].Points.Clear();
+            double y;
+
+            for (double x = a; x <= b; x += step)
+            {
+                y = Func(x);
+
+                chart1.Series[0].Points.AddXY(x, y);
+                if (x + step <= b)
+                {
+                    chart1.Series[0].Points.AddXY(x + step, y);
+                }
+
+            }
+        }
+
+        public static void DrawRightRect(ref Chart chart1, double a, double b, double step)
+        {
+            chart1.Series[0].Points.Clear();
+            double y;
+
+            for (double x = a; x <= b; x += step)
+            {
+                y = Func(x + step);
+
+                chart1.Series[0].Points.AddXY(x, y);
+                if (x + step <= b)
+                {
+                    chart1.Series[0].Points.AddXY(x + step, y);
+                }
+            }
+        }
+
+        public static void DrawIntegral(ref Chart chart1, double a, double b, double step)
+        {
+            chart1.Series[1].Points.Clear();
+            chart1.Series[0].BorderWidth = 3;
+
+            for (double i = a; i <= b; i += step)
+            {
+                double x = i;
+                double y = Func(i);
+
+                chart1.Series[1].Points.AddXY(x, y);
+            }
+        }
+    }
+}
---
+++ lab1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                double
+                    a = Convert.ToDouble(textBox1.Text),
+                    b = Convert.ToDouble(textBox2.Text),
+                    step = Convert.ToDouble(textBox3.Text);
+
+                label6.Text = Calculate.Trapezium(a, b, step).ToString();
+                Calculate.DrawTrapezium(ref chart1, a, b, step);
+                Calculate.DrawIntegral(ref chart1, a, b, step);
+                
+            }
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                double
+                    a = Convert.ToDouble(textBox1.Text),
+                    b = Convert.ToDouble(textBox2.Text),
+                    step = Convert.ToDouble(textBox3.Text);
+
+                label6.Text = Calculate.LeftRect(a, b, step).ToString();
+                Calculate.DrawLeftRect(ref chart1, a, b, step);
+                Calculate.DrawIntegral(ref chart1, a, b, step);
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                double
+                    a = Convert.ToDouble(textBox1.Text),
+                    b = Convert.ToDouble(textBox2.Text),
+                    step = Convert.ToDouble(textBox3.Text);
+
+                label6.Text = Calculate.RightRect(a, b, step).ToString();
+                Calculate.DrawRightRect(ref chart1, a, b, step);
+                Calculate.DrawIntegral(ref chart1, a, b, step);
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                double
+                     a = Convert.ToDouble(textBox1.Text),
+                     b = Convert.ToDouble(textBox2.Text),
+                     step = Convert.ToDouble(textBox3.Text);
+
+                label6.Text = Calculate.MiddleRect(a, b, step).ToString();
+                Calculate.DrawMiddleRect(ref chart1, a, b, step);
+                Calculate.DrawIntegral(ref chart1, a, b, step);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*chart1.ChartAreas[0].AxisX.Minimum = -0.1;
+            chart1.ChartAreas[0].AxisX.Maximum = 1.1;
+            chart1.ChartAreas[0].AxisY.Minimum = -0.1;
+            chart1.ChartAreas[0].AxisY.Maximum = 0.15;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = 0.05;
+            chart1.ChartAreas[0].AxisY.Minimum = Convert.ToDouble(textBox1.Text);
+            chart1.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(textBox2.Text);*/
+        }
+    }
+}
